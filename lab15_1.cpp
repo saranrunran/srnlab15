b572ba1ddcdbbfe5358153b137b0d9a0b14188f9
saranrunran <peemai.saranporn@gmail.com>
2026-02-03 01:29:17 +0700

    lab15_2completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,33 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	int *y = (int *)&b;
+	int **z = &x;
 
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+
+	cout << &a << " " << (int *)&b << " " << (int *)&c << " " << &x << " " << &y << " " << &z << endl;
 	
-	
+	c = 'F';
+
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+
+	*((char*)y) = 'W';
+
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+
+	*x = 6;
+
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+
+	**z = 7;
+
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+
 	return 0;
 }
