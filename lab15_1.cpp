1e2cc27c11f36ee434baf3078b8d96554de49a8f
saranrunran <peemai.saranporn@gmail.com>
2026-02-03 15:02:50 +0700

    lab15_1completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -6,28 +6,28 @@ int main(){
 	char b = 'A';
 	char &c = b;
 	int *x = &a;
-	int *y = (int *)&b;
+	char *y = &b;
 	int **z = &x;
 
-	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+	cout << a << " " << b << " " << c << " " << x << " " << &y << " " << z << endl;
 
 	cout << &a << " " << (int *)&b << " " << (int *)&c << " " << &x << " " << &y << " " << &z << endl;
 	
 	c = 'F';
 
-	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+	cout << a << " " << b << " " << c << " " << x << " " << &y << " " << z << endl;
 
-	*((char*)y) = 'W';
+	*y = 'W';
 
-	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+	cout << a << " " << b << " " << c << " " << x << " " << &y << " " << z << endl;
 
 	*x = 6;
 
-	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+	cout << a << " " << b << " " << c << " " << x << " " << &y << " " << z << endl;
 
 	**z = 7;
 
-	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+	cout << a << " " << b << " " << c << " " << x << " " << &y << " " << z << endl;
 
 	return 0;
 }
--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,27 @@ int main(){
 }
 
 //Do not modify source code above this line
+void shuffle(int &a,int &b,int &c,int &d) {
+	int y = rand()%4;
+	if(y == 0){
+		a = 50;
+		b = 100;
+		c = 500;
+		d = 1000;
+	} else if (y == 1) {
+		a = 100;
+		b = 500;
+		c = 1000;
+		d = 50;
+	} else if (y == 2) {
+		a = 500;
+		b = 1000;
+		c = 50;
+		d = 100;
+	} else if (y == 3) {
+		a = 1000;
+		b = 50;
+		c = 100;
+		d = 500;
+	}
+}
